298e9a03ca43d7d2064537fbd5706b226467ed0c
dfavretto <david.f@hotmail.com.ar>
2021-06-03 18:20:31 -0300

    Add validation for SwaggerFileName

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const swaggerHubApiNames = {
 }
 
 // const SWAGGER_HUB_API = process.env.SWAGGER_HUB_API;
-// const MODIFIED_FILES = JSON.parse(process.env.MODIFIED_FILES);
-const MODIFIED_FILES = [".github/workflows/blank.yml","README.md","index.js", "partner_bank_api.js"];
+const MODIFIED_FILES = JSON.parse(process.env.MODIFIED_FILES);
+// const MODIFIED_FILES = [".github/workflows/blank.yml","README.md","index.js", "oapi.js"];
 
 const getModifiedFilesByName = () => 
     MODIFIED_FILES.map((element) => element.split(/[\\\/]/).pop().split('.').shift())
-const getSwaggerHubApiName = () =>
+
+const getSwaggerFileName = () =>
     Object.keys(swaggerHubApiNames).find(element => getModifiedFilesByName().includes(element));
 
+const getSwaggerHubApiName = () =>
+    swaggerHubApiNames[getSwaggerFileName()];
+
 const readSwaggerDefinitionFile = () =>
-  fs.readFileSync(path.resolve(__dirname, `./${getSwaggerHubApiName()}.yaml`), 'utf8');
+  fs.readFileSync(path.resolve(__dirname, `./${getSwaggerFileName()}.yaml`), 'utf8');
 
-console.log(readSwaggerDefinitionFile());
-
+if (getSwaggerFileName() !== undefined) {
+    console.log(readSwaggerDefinitionFile());
+    console.log(getSwaggerHubApiName());
+}
